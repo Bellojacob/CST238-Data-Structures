af9e4daf4f9c53f61aa9ba2d27d97a5e55f25e13
Bellojacob <jacobbello2002@gmail.com>
2024-02-18 18:46:06 -0800

    HW 4 half complete

---
+++ Homework/Week4/src/Stack.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+/* Title: Stack.java
+ * Abstract: This program is added is a Stack Binary converter, we are starting with starting code from class and going
+ * to add a static method that takes in a number in base 10 (decimal or 26) and converts it to base 2 (binary or 10101). 
+ * Our method will be called convert.
+ * Author: Jacob Bello
+ * Email: jbello@csumb.edu
+ * Estimate: 1 hours
+ * Date: 02/18/2024
+ */
+public class Stack {
+    public static final int STACK_CAPACITY = 10;
+    private int [] data;
+    private int top;
+
+    public Stack() {
+        data = new int[STACK_CAPACITY];
+        top = -1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public void push(int value) {
+        if(top + 1 == data.length) {
+            System.out.println("**Stack full - cannot push " + value + "**");
+            return;
+        }
+        data[++top] = value;
+    }
+
+    public void pop() {
+        if(isEmpty()) {
+            System.out.println("**Stack empty - cannot pop**");
+            return;
+        }
+        // data[top] = null; // only if stack contains objects
+        top--;
+    }
+
+    public int top() {
+        if(isEmpty()) {
+            System.out.println("**Stack empty - returning garbage value**");
+            return 0;
+        }
+        return data[top];
+    }
+
+    public String toString() {
+        StringBuffer sb = new StringBuffer("Stack Contents: ");
+        if(isEmpty()) {
+            sb.append("Empty");
+        } else {
+            for (int i = top; i >= 0; i--) {
+                sb.append(data[i] + " ");
+            }
+        }
+        return sb.toString();
+    }
+    
+    public static String convert(int number){
+        Stack stack = new Stack();
+        while (number != 0) {
+            if (number % 2 == 1) {
+                stack.push(1);
+            } else if (number % 2 == 0) {
+                stack.push(0);
+            }
+            number = number/2;
+        }
+
+
+
+        // When done converting, use the stack and a StringBuffer to convert the stack contents to a string
+        StringBuffer sb = new StringBuffer();
+        while (!stack.isEmpty()){
+            sb.append(stack.top());
+            stack.pop();
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = 1;
+        Stack bin = new Stack();
+        while(number != 0) {
+            System.out.print("Enter a positive number (0 to quit): ");
+            number = in.nextInt();
+            if(number < 1) {
+                break;
+            }
+            System.out.print(number + " => ");
+            System.out.println(convert(number));
+        }
+        System.out.println("Exiting");
+    }
+}
