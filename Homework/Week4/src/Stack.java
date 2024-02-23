5828c8c73189eec0c7d8e194872923e805a747b3
Bellojacob <jacobbello2002@gmail.com>
2024-02-22 16:26:31 -0800

    Stack class completed with bio and edge cases. If number is greater than 3, ex: 4 or more, stack full will be presented. I was not instructed to change anything outside of making a new static method. 1.1

--- Homework/Week4/src/Stack.java
+++ Homework/Week4/src/Stack.java
@@ -1,13 +1,16 @@
-import java.util.Scanner;
 /* Title: Stack.java
  * Abstract: This program is added is a Stack Binary converter, we are starting with starting code from class and going
- * to add a static method that takes in a number in base 10 (decimal or 26) and converts it to base 2 (binary or 10101). 
+ * to add a static method that takes in a number in base 10 (decimal or 26) and converts it to base 2 (binary or 10101).
  * Our method will be called convert.
+ * Note: If number is greater than 3, ex: 4 or more (1234), stack full will be presented.
+ * I was not instructed to change anything in the instructions outside of making a new static method.
  * Author: Jacob Bello
  * Email: jbello@csumb.edu
  * Estimate: 1 hours
  * Date: 02/18/2024
  */
+import java.util.Scanner;
+
 public class Stack {
     public static final int STACK_CAPACITY = 10;
     private int [] data;
@@ -58,15 +61,19 @@ public class Stack {
         }
         return sb.toString();
     }
-    
+
+    // my binary converter method that takes an int number
     public static String convert(int number){
+        // create a new stack object
         Stack stack = new Stack();
         while (number != 0) {
+            // add the remainder to the stack
             if (number % 2 == 1) {
                 stack.push(1);
             } else if (number % 2 == 0) {
                 stack.push(0);
             }
+            // divide the number by 2 and loop
             number = number/2;
         }
 
@@ -81,19 +88,19 @@ public class Stack {
         return sb.toString();
     }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int number = 1;
-        Stack bin = new Stack();
-        while(number != 0) {
-            System.out.print("Enter a positive number (0 to quit): ");
-            number = in.nextInt();
-            if(number < 1) {
-                break;
-            }
-            System.out.print(number + " => ");
-            System.out.println(convert(number));
-        }
-        System.out.println("Exiting");
-    }
+    //    public static void main(String[] args) {
+    //        Scanner in = new Scanner(System.in);
+    //        int number = 1;
+    //        Stack bin = new Stack();
+    //        while(number != 0) {
+    //            System.out.print("Enter a positive number (0 to quit): ");
+    //            number = in.nextInt();
+    //            if(number < 1) {
+    //                break;
+    //            }
+    //            System.out.print(number + " => ");
+    //            System.out.println(convert(number));
+    //        }
+    //        System.out.println("Exiting");
+    //    }
 }
